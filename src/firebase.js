924c9087b42fcac96b05af0bff7f9d5ac5abf050
Xidhu <64782756+xidhu@users.noreply.github.com>
2021-04-02 15:28:27 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -19,15 +19,7 @@ import '@firebase/firestore';
 
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCCC1xR5Obq1DOwoqHbV3K4jaeq3P_BcIU",
-  authDomain: "chat3-f8994.firebaseapp.com",
-  databaseURL: "https://chat3-f8994.firebaseio.com",
-  projectId: "chat3-f8994",
-  storageBucket: "chat3-f8994.appspot.com",
-  messagingSenderId: "1091879896625",
-  appId: "1:1091879896625:web:6987ebc176213c9f58f710"
-};
+
 
  const firebaseApp = firebase.initializeApp(firebaseConfig);
 
